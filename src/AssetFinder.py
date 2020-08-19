8a3adce8ce2ecad1a4013b28ae8aedabad91bfba
snpunee <snpunee@amazon.com>
2020-08-19 23:10:22 +0530

    changed the list to set to avoid duplicate values for urls

--- src/AssetFinder.py
+++ src/AssetFinder.py
@@ -17,15 +17,15 @@ def fetch(url: str) -> dict:
     soup = BeautifulSoup(htmlbody, 'lxml')
     # getting all the img tags
     imgtags = soup.find_all('img')
-    # creating a list of src from the img tags
-    imgurls = [imgtag['src'] for imgtag in imgtags]
+    # creating a set of src from the img tags
+    imgurls = {imgtag['src'] for imgtag in imgtags}
     # getting all the a tags
     atags = soup.find_all('a')
-    # creating a list of links from the atags
-    aurll = [atag['href'] for atag in atags]
-    asset = {'assets': imgurls, 'links': aurll}
+    # creating a set of links from the atags
+    aurll = {atag['href'] for atag in atags}
+    asset = {'assets': list(imgurls), 'links': list(aurll)}
     return asset
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     fetch('https://google.com')
