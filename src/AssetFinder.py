10a3b6276d3f1cc3ee0cf57e63f8b3875a19a17c
snpunee <snpunee@amazon.com>
2020-08-20 01:36:18 +0530

    added the getWebsiteAssets function

--- src/AssetFinder.py
+++ src/AssetFinder.py
@@ -13,19 +13,49 @@ def fetch(url: str) -> dict:
     Returns:
 
     """
+
     htmlbody = requests.get(url).text
     soup = BeautifulSoup(htmlbody, 'lxml')
     # getting all the img tags
     imgtags = soup.find_all('img')
     # creating a set of src from the img tags
-    imgurls = {imgtag['src'] for imgtag in imgtags}
+    imgurls = {imgtag.get('src') for imgtag in imgtags}
     # getting all the a tags
     atags = soup.find_all('a')
     # creating a set of links from the atags
-    aurll = {atag['href'] for atag in atags}
+    aurll = {atag.get('href') for atag in atags if atag.get('href') and atag.get('href').startswith('http')}
     asset = {'assets': list(imgurls), 'links': list(aurll)}
     return asset
 
+def getWebsiteAssets(url: str)-> list:
+    """
+    function that returns list of image urls
+    Args:
+        url:
+
+    Returns:
+        list of image urls
+
+    """
+    urls = set()
+    imgurls = set()
+    result = fetch(url)
+    urls.update(result['links'])
+    imgurls.update(result['assets'])
+    for link in result['links']:
+        page = fetch(link)
+        imgurls.update(page['assets'])
+        temp = set()
+        temp.update(page['links'])
+        new_urls = urls - temp
+        urls.update(new_urls)
+        result['links'].extend(new_urls)
+        len(result['links'])
+
+    return imgurls
+
+
 
 if __name__ == '__main__':
-    fetch('https://google.com')
+    #fetch('https://google.com')
+    getWebsiteAssets('https://google.com')
