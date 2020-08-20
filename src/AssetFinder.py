1e3f70c8dd5864bf18c8be28b9235c22fe80a9af
snpunee <snpunee@amazon.com>
2020-08-20 09:12:31 +0530

    handled missing schema exception

--- src/AssetFinder.py
+++ src/AssetFinder.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+from requests.exceptions import MissingSchema
+import traceback
 
 
 def fetch(url: str) -> dict:
@@ -13,8 +15,15 @@ def fetch(url: str) -> dict:
     Returns:
 
     """
-    # todo add expection handling to make sure the code does not stops
-    htmlbody = requests.get(url).text
+    # todo handle request status codes
+    # only process requests with response 200
+    asset = {'assets': list(), 'links': list()}
+    try:
+        htmlbody = requests.get(url).text
+    except MissingSchema as ex:
+        print(traceback.format_exc())
+        return asset
+
     soup = BeautifulSoup(htmlbody, 'lxml')
     # getting all the img tags
     imgtags = soup.find_all('img')
@@ -23,11 +32,13 @@ def fetch(url: str) -> dict:
     # getting all the a tags
     atags = soup.find_all('a')
     # creating a set of links from the atags
+    # todo change logic to add the base url to relative links
     aurll = {atag.get('href') for atag in atags if atag.get('href') and atag.get('href').startswith('http')}
     asset = {'assets': list(imgurls), 'links': list(aurll)}
     return asset
 
-def getWebsiteAssets(url: str)-> list:
+
+def getWebsiteAssets(url: str) -> list:
     """
     function that returns list of image urls
     Args:
@@ -37,10 +48,9 @@ def getWebsiteAssets(url: str)-> list:
         list of image urls
 
     """
-    #todo add exception handling
-    #todo add logic to control depth
-    #todo try to implement this using recursion
-    #todo convert the image relative url to full urls
+    # todo add logic to control depth
+    # todo try to implement this using recursion
+    # todo convert the image relative url to full urls
 
     urls = set()
     imgurls = set()
@@ -52,17 +62,17 @@ def getWebsiteAssets(url: str)-> list:
     for link in result['links']:
         page = fetch(link)
         imgurls.update(page['assets'])
+        print(imgurls)
         temp = set()
         temp.update(page['links'])
         new_urls = urls - temp
         urls.update(new_urls)
         result['links'].extend(new_urls)
         len(result['links'])
-
     return imgurls
 
 
 
 if __name__ == '__main__':
     #fetch('https://google.com')
-    getWebsiteAssets('https://google.com')
+    getWebsiteAssets('www.udemy.com/')
